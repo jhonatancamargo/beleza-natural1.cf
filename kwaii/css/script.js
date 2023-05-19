fbc76c4f7405b9c70035bd5516114a10defb5564
jhonatancamargo <101683647+jhonatancamargo@users.noreply.github.com>
2023-05-19 07:40:13 -0300

    Update script.js

--- kwaii/css/script.js
+++ kwaii/css/script.js
@@ -32,7 +32,7 @@
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
   // Coloque o ID do vídeo do YouTube que você deseja exibir
-  var videoID = "cYL3r4tAY";
+  var videoID = "cYL3r4tAYX8";
   // Crie o player de vídeo do YouTube
   var player;
 
